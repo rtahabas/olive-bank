a4d31784250f2d48c258b7a72a25699ac7f20419
R.Taha BAS <rtahabas@RTaha-MacBook-Air.local>
2023-01-08 00:55:59 +0300

    Login hook invoke

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,44 +1,44 @@
-import { useState } from 'react'
-
-import styles from './Login.module.css'
+import { useLogin } from "../../hooks/useLogin";
+import styles from "./Login.module.css";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login,error, isPending } = useLogin();
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+    login(email, password);
+  };
 
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(e)
-    }
-
-    return (
-        <div className={styles.loginContainer}>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>Email:</span>
-                    <input
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        value={email}
-                    />
-                </label>
-                <label>
-                    <span>Password:</span>
-                    <input
-                        type="password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        value={password}
-                    />
-                </label>
-                <button className='btn'>
-                    Login
-                </button>
-            </form>
-        </div>
-    );
-}
+  return (
+    <div className={styles.loginContainer}>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Email:</span>
+          <input
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>Password:</span>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        {!isPending && <button className="btn">Login</button>}
+        {isPending && <button className="btn" disabled>Loading</button>}
+        {error && <p>{error}</p>}
+      </form>
+    </div>
+  );
+};
 
 export default Login;
