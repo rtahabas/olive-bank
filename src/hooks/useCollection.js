75322a80791bb158e58cf1ca3698bff68d1c8014
rtahabas <rtahabas@gmail.com>
2023-01-28 22:15:38 +0300

    useCollection hook added

---
+++ src/hooks/useCollection.js
@@ -0,0 +1,30 @@
+import { projectFireStore } from "../firebase/config";
+import { useEffect, useState } from "react";
+
+export const useCollection = (collection) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    let ref = projectFireStore.collection(collection);
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        let result = [];
+        snapshot.docs.forEach((doc) => {
+          result.push({ ...doc.data(), id: doc.id });
+        });
+
+        setDocument(result);
+        setError(null);
+      },
+      (error) => {
+        console.log(error);
+        setError("could not fetch the data");
+      }
+    );
+
+    return () => unsubscribe();
+  }, [collection]);
+
+  return { document, error };
+};
